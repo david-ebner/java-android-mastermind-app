11702f79e6828f4818e673bdd8fb5cdecc9c0086
Tobias Maier <2010653293@stud.fh-kufstein.ac.at>
2022-01-26 16:32:40 +0100

    Implemented GameOverDialogs and resized the Logo in MainActivity

--- app/src/main/java/fhku/appprojektmastermind/activity/GameActivity.java
+++ app/src/main/java/fhku/appprojektmastermind/activity/GameActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import fhku.appprojektmastermind.container.ColorGuessAdapter;
 import fhku.appprojektmastermind.container.ColorRepertoireView;
@@ -39,4 +45,40 @@ public class GameActivity extends AppCompatActivity {
         ColorRepertoireView colorRepertoireView = findViewById(R.id.colorRepertoire);
         colorRepertoireView.setColorRepertoire(game.getColorRepertoire());
     }
+
+    private void openLoseDialog() {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_lose);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        Button btn_lose_to_menu = dialog.findViewById(R.id.btn_lose_to_menu);
+        ImageView imageViewClose = dialog.findViewById(R.id.win_close);
+
+        btn_lose_to_menu.setOnClickListener(view -> backToMenu());
+
+        imageViewClose.setOnClickListener(view -> dialog.dismiss());
+
+        dialog.show();
+    }
+
+    private void openWinDialog() {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_win);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        Button btn_win_to_menu = dialog.findViewById(R.id.btn_win_to_menu);
+        ImageView imageViewClose = dialog.findViewById(R.id.win_close);
+
+        btn_win_to_menu.setOnClickListener(view -> backToMenu());
+
+        imageViewClose.setOnClickListener(view -> dialog.dismiss());
+
+        dialog.show();
+    }
+
+    private void backToMenu() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
+
