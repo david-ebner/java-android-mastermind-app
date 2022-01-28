246ec3e42273ed259de31d84b1273a92f7d5a1e5
Tobias Maier <2010653293@stud.fh-kufstein.ac.at>
2022-01-28 11:46:35 +0100

    created method for only getting PlayColors and implemented it

--- app/src/main/java/fhku/appprojektmastermind/MastermindGame.java
+++ app/src/main/java/fhku/appprojektmastermind/MastermindGame.java
@@ -2,6 +2,7 @@ package fhku.appprojektmastermind;
 
 import java.util.List;
 
+import fhku.appprojektmastermind.color.ColorBall;
 import fhku.appprojektmastermind.color.PresetColorBall;
 import fhku.appprojektmastermind.container.ColorGuess;
 import fhku.appprojektmastermind.container.ColorRepertoire;
@@ -10,6 +11,7 @@ public class MastermindGame {
     private final int COLOR_PATTERN_LENGTH;
     private final int ALLOWED_GUESS_ROUNDS;
     private final boolean ALLOW_DUPLICATES;
+    private final List<ColorBall> PLAY_COLORS;
 
     private final List<ColorGuess> COLOR_GUESS_ROUNDS;
     private final ColorRepertoire COLOR_REPERTOIRE;
@@ -22,15 +24,18 @@ public class MastermindGame {
         ALLOWED_GUESS_ROUNDS = allowedGuessRounds;
         ALLOW_DUPLICATES = allowDuplicates;
 
+        // set the Play Colors
+        PLAY_COLORS = PresetColorBall.getPlayColors();
+
         COLOR_GUESS_ROUNDS = ColorGuess.emptyGuessList(COLOR_PATTERN_LENGTH, ALLOWED_GUESS_ROUNDS);
         setupColorGuessListForTesting();
 
         // set up a ColorRepertoire containing all available PresetColorBalls
-        COLOR_REPERTOIRE = new ColorRepertoire(PresetColorBall.values());
+        COLOR_REPERTOIRE = new ColorRepertoire(PLAY_COLORS);
 
         COLOR_GUESS_ROUNDS.get(0).setActive();
 
-        TARGET_LIST = ColorGuess.createTargetList(COLOR_PATTERN_LENGTH, ALLOW_DUPLICATES);
+        TARGET_LIST = ColorGuess.createTargetList(COLOR_PATTERN_LENGTH, ALLOW_DUPLICATES, PLAY_COLORS);
     }
 
     public List<ColorGuess> getGuessRounds() {
@@ -74,9 +79,9 @@ public class MastermindGame {
     private void setupColorGuessListForTesting() {
         COLOR_GUESS_ROUNDS.set(0, new ColorGuess(List.of(
                 PresetColorBall.RED.getBall(),
-                PresetColorBall.BLUE.getBall(),
                 PresetColorBall.GREEN.getBall(),
-                PresetColorBall.BROWN.getBall()
+                PresetColorBall.BLUE.getBall(),
+                PresetColorBall.YELLOW.getBall()
         )));
         /*COLOR_GUESS_ROUNDS.set(1, new ColorGuess(List.of(
                 PresetColorBall.BROWN.getBall(),
