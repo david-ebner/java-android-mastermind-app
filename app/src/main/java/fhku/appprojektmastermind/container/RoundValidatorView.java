343fa020afaf22357280d325f4b9e42e1ddb3aea
David Ebner <2010653205@stud.fh-kufstein.ac.at>
2022-02-08 15:09:55 +0100

    Update RoundValidatorView. no need to overwrite everything

--- app/src/main/java/fhku/appprojektmastermind/container/RoundValidatorView.java
+++ app/src/main/java/fhku/appprojektmastermind/container/RoundValidatorView.java
@@ -8,8 +8,5 @@ public class RoundValidatorView extends ColorListView {
     public RoundValidatorView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.ROWS = 2;
-        this.VERTICAL_PADDING_FACTOR = .2;
-        this.START_PADDING_FACTOR = 2 * HORIZONTAL_PADDING_FACTOR;
-        this.TOP_PADDING_FACTOR = VERTICAL_PADDING_FACTOR;
     }
 }
